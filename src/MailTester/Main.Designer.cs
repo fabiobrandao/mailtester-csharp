1ca02ba8bcfae0bb7e193a15da9f56290a65ab37
Fábio Rodrigo Brandão <fabio@brainfonet.com>
2016-12-10 22:24:48 -0200

    First Commit

---
+++ src/MailTester/Main.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace MailTester
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txbStatus = new System.Windows.Forms.TextBox();
+            this.txbHost = new System.Windows.Forms.TextBox();
+            this.txbPort = new System.Windows.Forms.TextBox();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.txbPass = new System.Windows.Forms.TextBox();
+            this.txbFrom = new System.Windows.Forms.TextBox();
+            this.txbTo = new System.Windows.Forms.TextBox();
+            this.txbMessage = new System.Windows.Forms.TextBox();
+            this.txbSubject = new System.Windows.Forms.TextBox();
+            this.chkAutentication = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(13, 13);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(65, 13);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "SMTP Host:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(211, 13);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(62, 13);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "SMTP Port:";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(212, 50);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(32, 13);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "User:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(211, 74);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(33, 13);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Pass:";
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Location = new System.Drawing.Point(13, 166);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(46, 13);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "Subject:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(13, 116);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(33, 13);
+            this.lblFrom.TabIndex = 5;
+            this.lblFrom.Text = "From:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(13, 138);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(23, 13);
+            this.lblTo.TabIndex = 6;
+            this.lblTo.Text = "To:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 193);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(53, 13);
+            this.lblMessage.TabIndex = 7;
+            this.lblMessage.Text = "Message:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(13, 268);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Status:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(304, 253);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send Test";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(304, 371);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txbStatus
+            // 
+            this.txbStatus.Location = new System.Drawing.Point(16, 284);
+            this.txbStatus.Multiline = true;
+            this.txbStatus.Name = "txbStatus";
+            this.txbStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbStatus.Size = new System.Drawing.Size(363, 81);
+            this.txbStatus.TabIndex = 11;
+            // 
+            // txbHost
+            // 
+            this.txbHost.Location = new System.Drawing.Point(95, 13);
+            this.txbHost.Name = "txbHost";
+            this.txbHost.Size = new System.Drawing.Size(100, 20);
+            this.txbHost.TabIndex = 12;
+            // 
+            // txbPort
+            // 
+            this.txbPort.Location = new System.Drawing.Point(279, 13);
+            this.txbPort.Name = "txbPort";
+            this.txbPort.Size = new System.Drawing.Size(48, 20);
+            this.txbPort.TabIndex = 13;
+            // 
+            // txbUser
+            // 
+            this.txbUser.Location = new System.Drawing.Point(279, 47);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(100, 20);
+            this.txbUser.TabIndex = 14;
+            // 
+            // txbPass
+            // 
+            this.txbPass.Location = new System.Drawing.Point(279, 73);
+            this.txbPass.Name = "txbPass";
+            this.txbPass.PasswordChar = '*';
+            this.txbPass.Size = new System.Drawing.Size(100, 20);
+            this.txbPass.TabIndex = 15;
+            // 
+            // txbFrom
+            // 
+            this.txbFrom.Location = new System.Drawing.Point(95, 110);
+            this.txbFrom.Name = "txbFrom";
+            this.txbFrom.Size = new System.Drawing.Size(284, 20);
+            this.txbFrom.TabIndex = 16;
+            // 
+            // txbTo
+            // 
+            this.txbTo.Location = new System.Drawing.Point(95, 136);
+            this.txbTo.Name = "txbTo";
+            this.txbTo.Size = new System.Drawing.Size(284, 20);
+            this.txbTo.TabIndex = 17;
+            // 
+            // txbMessage
+            // 
+            this.txbMessage.Location = new System.Drawing.Point(95, 189);
+            this.txbMessage.Multiline = true;
+            this.txbMessage.Name = "txbMessage";
+            this.txbMessage.Size = new System.Drawing.Size(284, 58);
+            this.txbMessage.TabIndex = 18;
+            // 
+            // txbSubject
+            // 
+            this.txbSubject.Location = new System.Drawing.Point(95, 163);
+            this.txbSubject.Name = "txbSubject";
+            this.txbSubject.Size = new System.Drawing.Size(284, 20);
+            this.txbSubject.TabIndex = 19;
+            // 
+            // chkAutentication
+            // 
+            this.chkAutentication.AutoSize = true;
+            this.chkAutentication.Location = new System.Drawing.Point(95, 49);
+            this.chkAutentication.Name = "chkAutentication";
+            this.chkAutentication.Size = new System.Drawing.Size(110, 17);
+            this.chkAutentication.TabIndex = 20;
+            this.chkAutentication.Text = "Use Autentication";
+            this.chkAutentication.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 402);
+            this.Controls.Add(this.chkAutentication);
+            this.Controls.Add(this.txbSubject);
+            this.Controls.Add(this.txbMessage);
+            this.Controls.Add(this.txbTo);
+            this.Controls.Add(this.txbFrom);
+            this.Controls.Add(this.txbPass);
+            this.Controls.Add(this.txbUser);
+            this.Controls.Add(this.txbPort);
+            this.Controls.Add(this.txbHost);
+            this.Controls.Add(this.txbStatus);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "MailTester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txbStatus;
+        private System.Windows.Forms.TextBox txbHost;
+        private System.Windows.Forms.TextBox txbPort;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.TextBox txbPass;
+        private System.Windows.Forms.TextBox txbFrom;
+        private System.Windows.Forms.TextBox txbTo;
+        private System.Windows.Forms.TextBox txbMessage;
+        private System.Windows.Forms.TextBox txbSubject;
+        private System.Windows.Forms.CheckBox chkAutentication;
+    }
+}
+
---
+++ src/MailTester/Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace MailTester
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region ::ACTIONS::
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txbStatus.Text = string.Empty;
+
+            try
+            {
+                string smtpHost = txbHost.Text;
+                string smtpPort = txbPort.Text;
+                string smtpUser = txbUser.Text;
+                string smtPwd = txbPass.Text;
+
+                string strFrom = txbFrom.Text;
+                string strTo = txbTo.Text;
+
+                string strSubject = txbSubject.Text;
+                string strMensagem = txbMessage.Text;
+
+                txbStatus.Text += "\r\nSMTP Host: " + smtpHost;
+                txbStatus.Text += "\r\nSMTP Port: " + int.Parse(smtpPort);
+                txbStatus.Text += "\r\nSubject: " + strSubject;
+                txbStatus.Text += "\r\nMessage: " + strMensagem;
+                txbStatus.Text += "\r\nFrom: " + strTo;
+                txbStatus.Text += "\r\nTo: " + strFrom;
+
+                MailMessage mail = new MailMessage();
+                mail.Subject = strSubject;
+                mail.Body = strMensagem;
+                mail.IsBodyHtml = true;
+
+                mail.BodyEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+                mail.SubjectEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+
+                string from = strFrom;
+                mail.From = new System.Net.Mail.MailAddress(from);
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = smtpHost;
+                smtp.Port = int.Parse(smtpPort);
+
+                if (chkAutentication.Checked)
+                {
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = new System.Net.NetworkCredential(smtpUser, smtPwd);
+                }
+
+                mail.To.Add(strTo);
+
+                smtp.Send(mail);
+
+                txbStatus.Text += "\r\nSuccess!";
+            }
+            catch (Exception ex)
+            {
+                txbStatus.Text += "\r\nFail, details:  " + ex.Message;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txbStatus.Text = string.Empty;
+        }
+
+        #endregion
+    }
+}
